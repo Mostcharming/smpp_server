4eba63816c5574fe61e84c20ebe882a34144ab6f
Mayowa F Fredrick <mostcharming920@yahoo.com>
2025-02-05 12:06:14 +0100

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -12,7 +12,13 @@ const pool = mariadb.createPool({
   user: dbConfig.user,
   password: dbConfig.password,
   database: dbConfig.database,
-  port: dbConfig.port
+  port: dbConfig.port,
+  acquireTimeout: 20000, // Increase timeout to 20 seconds
+  idleTimeout: 30000, // Increase idle timeout
+  connectionLimit: 50, // Set the limit (adjust based on `max_connections`)
+  acquireTimeout: 20000, // Increase timeout (default is 10000ms)
+  waitForConnections: true, // Queue connections if all are in use
+  multipleStatements: true
 })
 
 module.exports = pool
